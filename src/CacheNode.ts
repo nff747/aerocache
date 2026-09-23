0e900c461756e9e2a6fb9715662ad54e57acf266
iKi <180210171+nff747@users.noreply.github.com>
2026-09-23 20:43:08 -0300

    add storage to CacheNode

--- src/CacheNode.ts
+++ src/CacheNode.ts
@@ -1,8 +1,10 @@
 export class CacheNode {
     private id: string;
+    private data: Map<string, any>;
     
     constructor(id: string) {
         this.id = id;
+        this.data = new Map();
     }
 
     getId(): string {
