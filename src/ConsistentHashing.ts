9aaa603678a78d971547c8d169181868a46b4aa4
iKi <180210171+nff747@users.noreply.github.com>
2026-09-23 20:43:24 -0300

    add consistent hashing math

--- src/ConsistentHashing.ts
+++ src/ConsistentHashing.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+
 export class ConsistentHashing {
     private ring: Map<number, string>;
     private keys: number[];
@@ -5,5 +7,10 @@ export class ConsistentHashing {
     constructor() {
         this.ring = new Map();
         this.keys = [];
+    }
+
+    private hash(key: string): number {
+        const hash = crypto.createHash('md5').update(key).digest('hex');
+        return parseInt(hash.substring(0, 8), 16);
     }
 }
